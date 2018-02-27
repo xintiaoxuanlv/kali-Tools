87a2e5759964cf68f25994570bfbc533e64a02da
{“text”：“json”} for GEEKEYES <36883093+nios34@users.noreply.github.com>
2018-02-27 19:19:17 +0800

    Add files via upload

--- Tools.py
+++ Tools.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*- 
+# -*- coding: UTF-8 -*-  
 #!/usr/bin/python3
 
 import os
@@ -7,7 +7,7 @@ import time
 from termcolor import *
 
 os.system('reset')
-print (colored('##################################################\n制作者：极客之眼团队_{"text":"json"}\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\n最近的一次更新时间：2018/2/27 13:45\n###########最后祝您使用愉快#######################',"green"))
+print (colored('##################################################\n制作者：极客之眼团队_{"text":"json"}\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\ngitub仓库:https://github.com/nios34/kali-Tools\n###########最后祝您使用愉快#######################',"green"))
 XX= input(colored('请写出您要执行的命令:',"yellow"))
 if XX == "help":
     print ("         install 安装程序")
@@ -24,6 +24,7 @@ if AZXX == "help":
     print (colored('          网易云音乐          未知',"blue"))
     print (colored('          输入法/PINYIN          OK',"blue"))
     print (colored('          补全系统          ok',"blue"))
+    print (colored('          w3af          ok',"blue"))
     AZXX = input(colored('请写出您要安装的程序：',"yellow"))
 if AZXX ==  "网易云音乐":
     os.system('wget -o music.deb http://d1.music.126.net/dmusic/netease-cloud-music_1.1.0_amd64_ubuntu.deb')
@@ -46,6 +47,15 @@ if AZXX == "PINYIN":
     os.system('python3 Tools.py')
 if AZXX == "补全系统":
     os.system('apt-get -y install kali-linux-all')
+    print (colored("操作完成，3秒后重启脚本","blue"))
+    time.sleep(3)
+    os.system('python3 Tools.py')
+if AZXX  == "w3af":
+    os.system('wget -P /home https://raw.githubusercontent.com/nios34/kali-Tools/%E5%B7%A5%E5%85%B7%E4%BE%9D%E8%B5%96/SSL%20for%20W3AF.py')
+    os.system('wget -P /home https://raw.githubusercontent.com/nios34/kali-Tools/master/w3af%E8%87%AA%E5%8A%A8%E5%AE%89%E8%A3%85.sh')
+    os.system('sh /home/w3af自动安装.sh')
+    os.system('rm -rf /home/w3af自动安装.sh')
+    os.system('rm -rf /home/SSL\ for\ W3AF.py')
     print (colored("操作完成，3秒后重启脚本","blue"))
     time.sleep(3)
     os.system('python3 Tools.py')
